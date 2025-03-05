8206af0804db9539e6f101e0e661e72642e06976
Ayaan Kaifullah <ayaanansari7921@outlook.com>
2025-03-05 15:58:36 -0500

    Migrate project to Chrome extension with Webpack and TypeScript

---
+++ extension/src/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  entry: {
+    popup: './src/popup.tsx',
+    content: './src/content.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              compilerOptions: {
+                noEmit: false,
+              },
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/manifest.json' },
+        { from: 'src/popup.html' },
+        { from: '../public/icon48.png' },
+        { from: '../public/icon128.png' },
+      ],
+    }),
+  ],
+}; 
